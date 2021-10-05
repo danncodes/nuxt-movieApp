9bd8dda109254cbaaec77b749ac588e76307d222
Daniel <daniel@personifyxp.com>
2021-10-05 10:50:24 +0100

    Initial Setup

---
+++ components/TheNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- Mobile Nav -->
+  <nav class="h-14 w-full rounded-sm fixed bottom-0 border-t border-opacity-20 shadow-inner
+sm:h-full sm:w-14 sm:bottom-[auto] sm:left-0 sm:border-r sm:border-b-0 duration-500
+flex justify-center sm:flex-col sm:justify-around">
+
+      <ul class="w-10/12 h-full sm:h-5/6 sm:w-full bg-white">
+
+      <!-- Home -->
+      <!-- Search -->
+      <!-- Watch List -->
+      <!-- Account / Sign Up / Login / Logout-->
+
+      </ul>
+
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="h-screen bg-[#202020]">
+    <TheNavbar />
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
