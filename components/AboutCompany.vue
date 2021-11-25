6f276514f8ab906dd050e30fef8a4bbe3ad31a5d
Daniel <daniel@personifyxp.com>
2021-11-25 00:11:04 +0000

    Production Fix ?

--- components/AboutCompany.vue
+++ components/AboutCompany.vue
@@ -1,5 +1,5 @@
 <template>
-<main v-if="company.logo_path" class="w-36">
+<main v-if="company.logo_path" class="w-36 max-w-[9rem]">
     <img class="object-cover h-full" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
 </main>
   
