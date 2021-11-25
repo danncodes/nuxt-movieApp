cb397faa43f4a90c87f02a85a171c0c1c0c58156
Daniel <daniel@personifyxp.com>
2021-11-25 00:23:07 +0000

    ...

--- components/AboutCompany.vue
+++ components/AboutCompany.vue
@@ -1,6 +1,6 @@
 <template>
 <main v-if="company.logo_path" class="min-w-[9rem] max-w-[9rem] h-full mx-1">
-    <img class="object-cover h-full" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
+    <img class="object-contain h-full" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
 </main>
   
 </template>
