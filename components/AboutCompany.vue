af8873ed330e06644c5fda881154f600e6fa6d73
Daniel <daniel@personifyxp.com>
2021-11-25 00:49:25 +0000

    ...

--- components/AboutCompany.vue
+++ components/AboutCompany.vue
@@ -1,6 +1,6 @@
 <template>
-<main v-if="company.logo_path" class="min-w-[9rem] max-w-[9rem] h-full mx-1">
-    <img class="object-contain h-full" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
+<main v-if="company.logo_path" class="mx-1">
+    <img class="object-contain max-h-[300px]" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
 </main>
   
 </template>
--- components/AboutProductionBlock.vue
+++ components/AboutProductionBlock.vue
@@ -5,7 +5,7 @@
     <h1 class="text-xl mb-4 font-medium uppercase border-l-4 border-[#0FEFFD] px-3 rounded-xs" v-if="blockType">{{ blockType }}</h1>
 
     <!-- Movies -->
-    <section class="flex justify-around items-center gap-3 max-h-40 max-w-full overflow-x-scroll" v-if="companies" ref="block">
+    <section class="flex justify-around items-center gap-3 max-w-full overflow-x-scroll" v-if="companies" ref="block">
         <AboutCompany v-for="company in companies" :key="company.id" :company="company"/>
     </section>
     <!-- Skeleton Loader -->
