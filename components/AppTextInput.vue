2e109b8dc026b4d0bcbca7d56bc655b1a5469f72
Daniel <daniel@personifyxp.com>
2021-11-27 12:44:43 +0000

    ...

--- components/AppTextInput.vue
+++ components/AppTextInput.vue
@@ -5,7 +5,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
     </div>
-    <input @input="userInput" type="text" :placeholder="placeholder" class="w-full p-2 pl-4 rounded rounded-l-none bg-gray-50 text-black text-xs outline-none duration-300 focus:shadow-md focus:border-t-4 border-[#0FEFFD]" ref="input">
+    <input @input="userInput" type="text" :placeholder="placeholder" class="w-full p-2 pl-4 rounded rounded-l-none bg-gray-50 text-black text-xs outline-none duration-300 focus:shadow-md focus:border-t-4 focus:textbase border-[#0FEFFD]" ref="input">
 
     <svg v-if="inputValue" @click="clearInput" xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 absolute right-2" fill="none" viewBox="0 0 24 24" stroke="black">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
