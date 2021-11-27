660bd4a236fca2ffa3126f8c56c905279ab0dd08
Daniel <daniel@personifyxp.com>
2021-11-27 02:54:58 +0000

    Search Field Setup

--- components/AppSearch.vue
+++ components/AppSearch.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="h-[calc(100vh-3.5rem)] w-full sm:h-screen sm:w-[calc(100%-3.5rem)] ml-auto fixed top-0 sm:right-0 z-50 bg-theme-primary duration-500"
-    :class="!searchScreen ? '-translate-y-full' : 'translate-y-0'" ></main>
+  <main class="h-[calc(100vh-3.5rem)] w-full sm:h-screen sm:w-[calc(100%-3.5rem)] ml-auto fixed top-0 sm:right-0 z-50 bg-theme-primary duration-500 p-4"
+    :class="!searchScreen ? '-translate-y-full' : 'translate-y-0'" >
+    <AppTextInput placeholder="Search for a Movie or Genre ..." />
+  </main>
 </template>
 
 <script>
---
+++ components/AppTextInput.vue
@@ -0,0 +1,40 @@
+<template>
+<main class="w-full my-4 relative flex items-center h-8 max-w-md mx-auto">
+    <div class="bg-gray-50 h-full flex items-center rounded-l p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+    </div>
+    <input @input="userInput" type="text" :placeholder="placeholder" class="w-full p-2 pl-4 rounded rounded-l-none bg-gray-50 text-black text-xs outline-none duration-300 focus:shadow-md focus:border-t-4 border-[#0FEFFD]" ref="input">
+
+    <svg v-if="inputValue" @click="clearInput" xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 absolute right-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+</main>
+  
+</template>
+
+<script>
+export default {
+    props: ["placeholder"],
+    data(){
+        return {
+            inputValue: ""
+        }
+    },
+    methods: {
+        clearInput(){
+            this.$refs.input.value = ""
+            this.inputValue = this.$refs.input.value
+        },
+        userInput(){
+            this.inputValue = this.$refs.input.value
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
