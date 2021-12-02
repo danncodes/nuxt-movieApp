73c15411423acdb9c44d266eea98f98aca42732e
Daniel <daniel@personifyxp.com>
2021-12-02 00:19:16 +0000

    ....

--- components/AboutCompany.vue
+++ components/AboutCompany.vue
@@ -1,6 +1,6 @@
 <template>
 <main v-if="company.logo_path" class="mx-1">
-    <img class="object-contain max-h-[300px] max-w-xs" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
+    <img class="object-contain max-h-[150px] lg:max-h-[300px] max-w-[10rem] lg:max-w-xs" :src="'https://image.tmdb.org/t/p/original'+ company.logo_path" alt="">
 </main>
   
 </template>
