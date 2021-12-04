b5b78a81bd15d82f5b124b64cfaf1175ad01eb66
Daniel <daniel@personifyxp.com>
2021-12-04 19:43:06 +0000

    Iniitial Because you Liked .. Set Up

--- pages/favourites.vue
+++ pages/favourites.vue
@@ -1,16 +1,14 @@
 <template>
   <main class="min-h-screen">
-    
-    <!-- Recently Viewed??  -->
-
-
     <!-- No Favouraites -->
     <section class="h-96 w-full flex justify-center" v-if="!favouriteMovies.length">
       <h2 class="my-4">No Favouraites</h2>
     </section>
 
+    <!-- User Favourites -->
     <section v-else class="">
       <AppMoviesBlock blockType="Favourites" :movies="favouriteMovies"/>
+      <AppMoviesBlock :blockType="'Because you like ' + movies.id" :movies="movies.movies" v-for="movies in similarMovies" :key="movies.id"/>
     </section>
   </main>
 </template>
@@ -19,28 +17,52 @@
 export default {
   data(){
     return {
-      favouriteMovies: []
+      favouriteMovies: [],
+      similarMovies: []
     }
   },
   mounted(){
     this.getFavourites()
+    this.getSimlarMovies()
   },
   methods: {
     getFavourites(){
-        this.favourites.forEach( (movie) => {
-        this.getMovie(movie)
+      console.log(this.favourites)
+      console.log(this.favourites.length)
+      this.favourites.forEach( (movie) => {
+      this.getMovie(movie)
+      this.getSimlarMovies(movie)
       })
+      console.log(this.similarMovies)
     },
     async getMovie(movie){
-            try{
-              const req = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${this.apiKey}&language=en-US`)
-              const data = await req.json()
-              this.favouriteMovies.push(data)
-            }
-            catch(e){
-              console.log(e.message)
-            }
-        },
+      try{
+        const req = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${this.apiKey}&language=en-US`)
+        const data = await req.json()
+        this.favouriteMovies.push(data)
+      }
+      catch(e){
+        console.log(e.message)
+      }
+    },
+    async getSimlarMovies(movie){
+      try{
+        const req = await fetch(`https://api.themoviedb.org/3/movie/${movie}/similar?api_key=${this.apiKey}&language=en-US`)
+        const data = await req.json()
+        
+        if(!data.results) return
+        console.log(movie, "HERE")
+        const movies = {
+          movies: data.results,
+          id: movie
+        }
+        this.similarMovies.push(movies)
+        console.log(movies)
+      }
+      catch(e){
+        console.log(e.message)
+      }
+    }
   },
   computed:{
     favourites(){
