82d53774a68e032c23d26ca6e52b0000f5ac18be
Daniel <daniel@personifyxp.com>
2021-12-06 22:10:25 +0000

    End Point Update

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -28,8 +28,8 @@
     </li>
 
     <!-- Generes -->
-    <li @click="closeSearchScreen" :class="{ 'sm:border-l-4 sm:border-b-0 border-b-4 border-[#0FEFFD] rounded': this.$route.name === 'genres' && !searchScreen }">
-        <NuxtLink to="/genres" class="sm:w-full w-14 flex justify-center items-center sm:h-14 h-full hover:bg-white cursor-pointer hover:bg-opacity-30 duration-300">
+    <li @click="closeSearchScreen" :class="{ 'sm:border-l-4 sm:border-b-0 border-b-4 border-[#0FEFFD] rounded': this.$route.name === 'watch-providers' && !searchScreen }">
+        <NuxtLink to="/watch-providers" class="sm:w-full w-14 flex justify-center items-center sm:h-14 h-full hover:bg-white cursor-pointer hover:bg-opacity-30 duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
             </svg>
